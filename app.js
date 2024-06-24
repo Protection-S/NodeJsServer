704e3b83352d63b42add5b6fdd30705178bcd8aa
Maxim <anarhisti2000@gmail.com>
2024-06-24 21:10:04 +1000

    Улучшенный сервер

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const http = require("http");
-http.createServer(function(request,response){
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/static', (request, response) => {
+  response.json({ header: 'Hello', body: 'Octagon NodeJS Test' });
+});
+
+app.get('/', (request, response) => {
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
     response.end('<h1>Привет, Октагон!</h1>');
-     
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
+  });
+
+app.get('/dynamic', (request, response) => {
+  const a = parseFloat(request.query.a);
+  const b = parseFloat(request.query.b);
+  const c = parseFloat(request.query.c);
+
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    response.json({ header: 'Error' });
+  } else {
+    const result = (a * b * c) / 3;
+    response.json({ header: 'Calculated', body: result.toString() });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
